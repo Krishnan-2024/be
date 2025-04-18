3b7d40e4c993f932359e65f2d366a20a8a4bd913
Krishnan S <sivakrishnas785@gmail.com>
2025-04-18 16:46:47 +0530

    Update settings.py

--- employee_tracking/settings.py
+++ employee_tracking/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = BASE_DIR/"uploads"
-MEDIA_URl = "/media/"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -170,6 +170,7 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     'https://frontend-tfqc.onrender.com',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
